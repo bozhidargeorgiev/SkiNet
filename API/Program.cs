1691bb576110d0712fb0f49d6d3720d85590ae26
Bozhidar Georgiev <bozhidar@winwin.ie>
2026-05-12 15:47:20 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
@@ -22,5 +25,19 @@ var app = builder.Build();
 // }
 
 app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    context.Database.Migrate();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
 
 app.Run();
