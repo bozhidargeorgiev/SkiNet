0dc312afcfbc58657b227b255cf32662bed6d842
Bozhidar Georgiev <bozhidar@winwin.ie>
2026-05-17 21:55:51 +0100

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
@@ -24,7 +26,11 @@ var app = builder.Build();
 // {
 //     app.MapOpenApi();
 // }
-
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins(
+    "http://localhost:5300",
+    "https://localhost:5301"
+));
 app.MapControllers();
 
 try
